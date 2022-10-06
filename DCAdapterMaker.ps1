611174de5c26993ddc2550bcb98a54533aa532ff
bschirk <91571473+bschirk@users.noreply.github.com>
2022-10-05 21:25:39 -0400

    Update DCAdapterMaker.ps1

--- DCAdapterMaker.ps1
+++ DCAdapterMaker.ps1
@@ -1,5 +1,5 @@
 #Create a new task called DCAdapter
-$action = New-ScheduledTaskAction -Execute 'C:\DC\scripts\DairycompAdapter.bat' -Argument /qn #Specify the script to run
+$action = New-ScheduledTaskAction -Execute 'C:\ProgramData\DC\scripts\DairycompAdapter.bat' -Argument /qn #Specify the script to run
 $trigger = New-ScheduledTaskTrigger -AtLogon #Specify the trigger to run the script
 $Principal = New-ScheduledTaskPrincipal -UserID "$env:USERDOMAIN\$env:USERNAME" -LogonType ServiceAccount -RunLevel Highest
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "DCAdapter" -Description "Names DairyComp Adapter." -Principal $Principal
