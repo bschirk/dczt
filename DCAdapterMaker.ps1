ed347354a39a9e47153930c73dbe3ebfc4454afe
bschirk <91571473+bschirk@users.noreply.github.com>
2022-10-06 11:32:46 -0400

    Update DCAdapterMaker.ps1

--- DCAdapterMaker.ps1
+++ DCAdapterMaker.ps1
@@ -2,10 +2,6 @@
 $action = New-ScheduledTaskAction -Execute 'C:\ProgramData\DC\Scripts\DairycompAdapter.bat' -Argument /qn #Specify the script to run
 $trigger = New-ScheduledTaskTrigger -AtLogon #Specify the trigger to run the script
 $Principal = New-ScheduledTaskPrincipal -UserID "$env:USERDOMAIN\$env:USERNAME" -LogonType ServiceAccount -RunLevel Highest
-md C:\\ProgramData\DC\ | Out-Null
-Start-BitsTransfer -Source "https://download.zerotier.com/dist/ZeroTier%20One.msi" -Destination "C:\\ProgramData\DC\ZerotierOne.msi" | Out-Null
-MsiExec.exe /i "C:\\ProgramData\DC\ZerotierOne.msi" /qn
-Set-NetConnectionProfile -InterfaceAlias Ethernet* -NetworkCategory "Private" | Out-Null
 md C:\\ProgramData\DC\Scripts | Out-Null
 cd C:\\ProgramData\DC\Scripts | Out-Null
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/bschirk/dczt/main/DairycompAdapter.bat -OutFile C:\\ProgramData\DC\Scripts\DairycompAdapter.bat | Out-Null
